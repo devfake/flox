2b062cee7c041cffccfbf528719789b100a53581
devfake <devfakeplus@googlemail.com>
2015-07-30 14:07:13 +0200

    simple lazy loading

--- client/app/sites/category.js
+++ client/app/sites/category.js
@@ -7,7 +7,9 @@ class Category extends React.Component {
   state = {
     category: {},
     items: [],
-    currentLoaded: config.loadingItems
+    currentLoaded: config.loadingItems,
+    moreLoaded: false,
+    moreToLoad: true
   }
 
   componentWillReceiveProps(nextProps) {
@@ -27,11 +29,24 @@ class Category extends React.Component {
 
   // todo: fix icon load.
   render() {
+    let loadContent;
+
+    if( ! this.state.moreLoaded) {
+      loadContent = <div className="load-more" onClick={this.loadMore.bind(this)}>Load more {this.state.category.name}</div>
+    } else {
+      loadContent = <i className="icon-load-more"></i>
+    }
+
     return (
 
       <div>
-        { ! this.state.items.length ? <i className="icon-content-load"></i> : <Box items={this.state.items} category={this.state.category} type="category" currentLoaded={this.state.currentLoaded} />}
-        {this.state.items.length >= config.loadingItems ? <div className="wrap"><div className="load-more" onClick={this.loadMore.bind(this)}>Load more {this.state.category.name}</div></div> : ''}
+        {this.state.items.length
+          ? <Box items={this.state.items} category={this.state.category} type="category" currentLoaded={this.state.currentLoaded} />
+          : <i className="icon-content-load"></i>}
+
+        {this.state.items.length >= config.loadingItems && this.state.moreToLoad
+          ? <div className="wrap"><div className="load-more-wrap">{loadContent}</div></div>
+          : ''}
       </div>
 
     );
@@ -49,12 +64,22 @@ class Category extends React.Component {
   }
 
   loadMore() {
-    Api.moreCategoryItems(this.state.category, this.state.currentLoaded).then((value) => {
-      this.setState({
-        currentLoaded: this.state.currentLoaded + config.loadingItems,
-        items: this.state.items.concat(value)
-      });
+    this.setState({
+      moreLoaded: true
     });
+
+    setTimeout(() => {
+
+      Api.moreCategoryItems(this.state.category, this.state.currentLoaded).then((value) => {
+        this.setState({
+          currentLoaded: this.state.currentLoaded + config.loadingItems,
+          items: this.state.items.concat(value),
+          moreLoaded: false,
+          moreToLoad: value.length ? true : false
+        });
+      });
+
+    }, 400);
   }
 }
 
