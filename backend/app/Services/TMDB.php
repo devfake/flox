8e41b24f45bc1b312a203b46b9305293d1c87b75
devfake <devfakeplus@googlemail.com>
2016-11-24 08:52:14 +0100

    update genre list

--- backend/app/Services/TMDB.php
+++ backend/app/Services/TMDB.php
@@ -193,6 +193,14 @@
         53 => 'Thriller',
         10752 => 'War',
         37 => 'Western',
+        10759 => 'Action & Adventure',
+        10762 => 'Kids',
+        10763 => 'News',
+        10764 => 'Reality',
+        10765 => 'Sci-Fi & Fantasy',
+        10766 => 'Soap',
+        10767 => 'Talk',
+        10768 => 'War & Politics',
       ];
     }
   }
