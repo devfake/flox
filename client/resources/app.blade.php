c206997a84abc7aa94542010857e1cea56e35448
devfake <devfakeplus@googlemail.com>
2016-11-28 15:29:58 +0100

    update page title

--- client/resources/app.blade.php
+++ client/resources/app.blade.php
@@ -6,7 +6,7 @@
   <meta id="token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
 
-  <title>Flox - Collect Your Movie Watch List</title>
+  <title>Flox - Collect Your Movie, Series and Anime Watch List</title>
   <link rel="stylesheet" href="{{ url('assets/app.css') }}">
   <link href="{{ url('assets/favicon.ico?v=3') }}" rel="icon" type="image/x-icon">
 
