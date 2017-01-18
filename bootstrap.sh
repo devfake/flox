6f012ed7c223a7e720dfe71195eecd8ed428b2ce
Tim Meier <raco0n@gmx.de>
2017-01-18 19:45:48 +0100

    Update Vagrantfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 DOCUMENT_ROOT='/vagrant/public' # change if using a subdirectory in your project
-SMTP_RELAY='1.2.3.4'
 
 ### Bookeeping ###
 mkdir -p /root/.provisioning
@@ -10,7 +9,7 @@ mkdir -p /root/.provisioning
 apt-add-repository ppa:ondrej/php -y
 apt-get update -y
 apt-get install -y apache2
-apt-get install -y php7.0-cli php7.0-common libapache2-mod-php7.0 php7.0 php7.0-mysql php7.0-fpm php7.0-curl php7.0-gd php7.0-bz2 php7.0-mbstring php-xml phpunit php7.0-sqlite
+apt-get install -y php7.0-cli php7.0-common libapache2-mod-php7.0 php7.0 php7.0-mysql php7.0-fpm php7.0-curl php7.0-gd php7.0-bz2 php7.0-mbstring php7.0-xml phpunit php7.0-sqlite
 
 cat <<CONFIG > /etc/apache2/sites-enabled/000-default.conf
 <VirtualHost *:80>
@@ -33,7 +32,10 @@ CONFIG
 
 sudo ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/
 sudo sed -i 's/^User.*/User vagrant/g' /etc/apache2/apache2.conf 
-php7enmod mcrypt
+sudo echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
+sudo a2dismod php5
+sudo a2enmod php7.0
 
 service apache2 restart
 update-rc.d apache2 defaults
@@ -50,9 +52,3 @@ mysql -u root <<-MSQL
 	GRANT ALL PRIVILEGES ON *.* TO root@'%';
 	FLUSH PRIVILEGES;
 MSQL
-
-### SMTP RELAY ###
-DEBIAN_FRONTEND=noninteractive apt-get -y install mailutils
-sed -i "s/^relayhost.*/relayhost = ${SMTP_RELAY}/g" /etc/postfix/main.cf
-service postfix restart
-update-rc.d postfix defaults
