ef97495c9b44a50157ea92d614107c91478ce970
devfake <devfakeplus@googlemail.com>
2017-01-23 10:17:38 +0100

    item service

--- backend/app/Services/Models/ItemService.php
+++ backend/app/Services/Models/ItemService.php
@@ -11,28 +11,28 @@
     private $model;
     private $tmdb;
     private $storage;
-    private $alternativeTitle;
-    private $episode;
+    private $alternativeTitleService;
+    private $episodeService;
 
     /**
      * @param Model                   $model
      * @param TMDB                    $tmdb
      * @param Storage                 $storage
-     * @param AlternativeTitleService $alternativeTitle
-     * @param EpisodeService          $episode
+     * @param AlternativeTitleService $alternativeTitleService
+     * @param EpisodeService          $episodeService
      */
     public function __construct(
       Model $model,
       TMDB $tmdb,
       Storage $storage,
-      AlternativeTitleService $alternativeTitle,
-      EpisodeService $episode
+      AlternativeTitleService $alternativeTitleService,
+      EpisodeService $episodeService
     ){
       $this->model = $model;
       $this->tmdb = $tmdb;
       $this->storage = $storage;
-      $this->alternativeTitle = $alternativeTitle;
-      $this->episode = $episode;
+      $this->alternativeTitleService = $alternativeTitleService;
+      $this->episodeService = $episodeService;
     }
 
     /**
@@ -45,11 +45,73 @@
 
       $this->storage->downloadPoster($item->poster);
 
-      $this->episode->create($item);
+      $this->episodeService->create($item);
 
-      $this->alternativeTitle->create($item);
+      $this->alternativeTitleService->create($item);
 
       return $item;
+    }
+
+    /**
+     * @param $itemId
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    public function remove($itemId)
+    {
+      $item = $this->model->find($itemId);
+
+      if( ! $item) {
+        return response('Not Found', 404);
+      }
+
+      $tmdbId = $item->tmdb_id;
+
+      $item->delete();
+
+      // Delete all related episodes, alternative titles and poster image.
+      $this->episodeService->remove($tmdbId);
+      $this->alternativeTitleService->remove($tmdbId);
+      $this->storage->removePosterFile($item->poster);
+    }
+
+    /**
+     * Return all items with pagination.
+     *
+     * @param $type
+     * @param $orderBy
+     * @return mixed
+     */
+    public function getWithPagination($type, $orderBy)
+    {
+      $orderType = $orderBy == 'rating' ? 'asc' : 'desc';
+
+      $item = $this->model->orderBy($orderBy, $orderType)->with('latestEpisode');
+
+      if($type != 'home') {
+        $item = $item->where('media_type', $type);
+      }
+
+      return $item->simplePaginate(config('app.LOADING_ITEMS'));
+    }
+
+    /**
+     * Update rating for an movie.
+     *
+     * @param $itemId
+     * @param $rating
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    public function changeRating($itemId, $rating)
+    {
+      $item = $this->model->find($itemId);
+
+      if( ! $item) {
+        return response('Not Found', 404);
+      }
+
+      $item->update([
+        'rating' => $rating,
+      ]);
     }
 
     /**
