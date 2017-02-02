23fa806a8230ce360624d1dbeee5b616953b4d9f
devfake <devfakeplus@googlemail.com>
2017-02-02 16:48:36 +0100

    remove migration comments

--- backend/database/migrations/2017_02_01_122243_change_tmdb_id_field.php
+++ backend/database/migrations/2017_02_01_122243_change_tmdb_id_field.php
@@ -6,11 +6,6 @@ use Illuminate\Database\Migrations\Migration;
 
 class ChangeTmdbIdField extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('items', function (Blueprint $table) {
@@ -18,11 +13,6 @@ class ChangeTmdbIdField extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('items', function (Blueprint $table) {
--- backend/database/migrations/2017_02_02_080109_add_fp_name_field_to_episodes_and_items.php
+++ backend/database/migrations/2017_02_02_080109_add_fp_name_field_to_episodes_and_items.php
@@ -6,11 +6,6 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddFPNameFieldToEpisodesAndItems extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('items', function (Blueprint $table) {
@@ -22,11 +17,6 @@ class AddFPNameFieldToEpisodesAndItems extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('items', function (Blueprint $table) {
