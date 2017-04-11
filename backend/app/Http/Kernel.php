342683adebaa006298b709bdc2dc96756d37e86c
Viktor Geringer <devfakeplus@googlemail.com>
2017-04-11 11:28:58 +0200

    disable csrf (#68)

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -28,7 +28,6 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
@@ -52,5 +51,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'csrf' => \App\Http\Middleware\VerifyCsrfToken::class,
     ];
 }
