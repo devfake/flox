0972b8d9d062d483b3e13eb9ccb631b5211aae00
devfake <devfakeplus@googlemail.com>
2017-08-15 10:14:36 +0200

    show if episode has no release date yet

--- client/app/components/Modal/Season.vue
+++ client/app/components/Modal/Season.vue
@@ -33,6 +33,7 @@
         <span class="modal-name" :class="{'spoiler-protect': spoiler && ! episode.seen && auth}">{{ episode.name }}</span>
         <i class="item-has-src" v-if="episode.src"></i>
         <span class="modal-release-episode" v-if="episode.release_episode_human_format" :title="released(episode.release_episode)"><i></i> {{ episode.release_episode_human_format }}</span>
+        <span class="modal-release-episode" v-if=" ! episode.release_episode"><i></i> {{ lang('no release') }}</span>
         <span class="episode-seen" :class="{seen: episode.seen}"><i></i></span>
       </div>
     </div>
