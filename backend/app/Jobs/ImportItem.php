f14bd940003b5391caeebebb41a7315f2ef69388
devfake <devfakeplus@googlemail.com>
2018-09-14 22:38:48 +0200

    show propper messages for queue jobs

--- backend/app/Jobs/ImportItem.php
+++ backend/app/Jobs/ImportItem.php
@@ -10,7 +10,6 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Symfony\Component\HttpFoundation\Response;
 
 class ImportItem implements ShouldQueue
 {
