3335f276eac5103c3be901f36d340bbe794b089b
devfake <devfakeplus@googlemail.com>
2019-12-22 14:19:16 +0100

    bump to 2.0.0

--- backend/config/app.php
+++ backend/config/app.php
@@ -3,7 +3,7 @@
   return [
 
     // Current version. Is synced with git releases.
-    'version' => '1.2.2',
+    'version' => '2.0.0',
 
     'TRANSLATION' => env('TRANSLATION', 'EN'),
     'LOADING_ITEMS' => env('LOADING_ITEMS'),
