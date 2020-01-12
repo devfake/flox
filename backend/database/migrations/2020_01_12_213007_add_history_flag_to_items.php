6571319d2e96eb454ee370bb6430cb1525171a50
Andreas Gaisbauer <a.gaisbauer@altimate.net>
2020-01-12 23:03:19 +0100

    added is_historic-flag to items

---
+++ backend/database/migrations/2020_01_12_213007_add_history_flag_to_items.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Item;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddHistoryFlagToItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->boolean('is_historic')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
