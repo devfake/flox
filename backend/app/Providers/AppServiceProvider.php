8ea2bdab7429d0c56bf13e1e8d1e7782e571b4d5
bloomvdomino <informatikerhb@gmail.com>
2020-07-16 19:45:34 +0200

    Enfore HTTPS for reverse proxy usage

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      $this->app['request']->server->set('HTTPS', true);
+      URL::forceScheme('https');
     }
 
     /**
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('APP_ENV') === 'production') {
+           $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
