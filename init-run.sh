7dc56864b0fd838f2b613645b63b876d2ebac8cc
olofvndrhr <68583543+olofvndrhr@users.noreply.github.com>
2021-02-18 19:40:07 +0100

    Update init-run.sh
    added mail support

--- init-run.sh
+++ init-run.sh
@@ -3,6 +3,11 @@ php artisan flox:init --no-interaction -vvv ${FLOX_DB_NAME} ${FLOX_DB_USER} ${FL
 sed -ri -e 's!TMDB_API_KEY=!TMDB_API_KEY=${TMDB_API_KEY}!g' .env
 sed -ri -e 's!TRANSLATION=!TRANSLATION=${FLOX_TRANSLATION}!g' .env
 sed -ri -e 's!APP_URL=http://localhost!APP_URL=${FLOX_APP_URL:-http://localhost}!g' .env
+sed -ri -e 's!MAIL_DRIVER=smtp!MAIL_DRIVER=${MAIL_DRIVER}!g' .env
+sed -ri -e 's!MAIL_HOST=smtp.mailtrap.io!MAIL_HOST=${MAIL_HOST}!g' .env
+sed -ri -e 's!MAIL_PORT=2525!MAIL_PORT=${MAIL_PORT}!g' .env
+sed -ri -e 's!MAIL_USERNAME=null!MAIL_USERNAME=${MAIL_USERNAME}!g' .env
+sed -ri -e 's!MAIL_PASSWORD=null!MAIL_PASSWORD=${MAIL_PASSWORD}!g' .env
+sed -ri -e 's!MAIL_ENCRYPTION=null!MAIL_ENCRYPTION=${MAIL_ENCRYPTION}!g' .env
 php artisan flox:db --no-interaction -vvv ${FLOX_ADMIN_USER} ${FLOX_ADMIN_PASS}
 chown -R ${APACHE_RUN_USER}:${APACHE_RUN_GROUP} /var/www/html
-
