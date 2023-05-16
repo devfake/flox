cea991bda39ad1a0b835d0e47fd3e0b56fb4d9a8
Simounet <contact@simounet.net>
2023-05-16 21:56:34 +0200

    IMDb rating broken web scraping method removed

--- backend/app/Services/IMDB.php
+++ backend/app/Services/IMDB.php
@@ -2,26 +2,13 @@
 
   namespace App\Services;
 
-  use DiDom\Document;
-
   class IMDB {
-
-    private $document;
-
-    public function __construct(Document $document)
-    {
-      $this->document = $document;
-    }
 
     public function parseRating($id = null)
     {
-      $document = $this->document->loadHtmlFile(config('services.imdb.url') . $id);
-
-      // We don't need to check if we found a result if we loop over them.
-      foreach($document->find('.ratingValue strong span') as $rating) {
-        return $rating->text();
-      }
-
+      // Web scraping is not working anymore. We should use the IMDb API instead
+      // Keeping the code here in the meantime
+      // config('services.imdb.url') . $id
       return null;
     }
   }
