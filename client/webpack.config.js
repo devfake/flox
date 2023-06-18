dce8c78417d2076bd426050d4304eea621175d0c
Simounet <contact@simounet.net>
2023-06-18 22:45:57 +0200

    Vue2 updated from 2.5.2 to 2.7.14

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,72 +1,77 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
   entry: {
-    app: './app/app.js',
-    vendor: ['vue', 'axios', 'vuex', 'debounce', 'vue-router']
+    app: "./app/app.js",
+    vendor: ["vue", "axios", "vuex", "debounce", "vue-router"],
   },
   watchOptions: {
-    poll: true
+    poll: true,
   },
   output: {
-    path: path.resolve('../public/assets'),
-    filename: 'app.js'
+    path: path.resolve("../public/assets"),
+    filename: "app.js",
   },
   resolve: {
     alias: {
-      vue$: 'vue/dist/vue.common'
-    }
+      vue$: "vue/dist/vue.common",
+    },
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        use: 'vue-loader'
+        use: "vue-loader",
       },
       {
         test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
+        use: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|jpg|svg|woff|woff2|eot|ttf)$/,
         use: {
-          loader: 'url-loader',
+          loader: "url-loader",
           options: {
             limit: 10000,
-            name: 'img/[name].[ext]',
-            emitFile: false
-          }
-        }
+            name: "img/[name].[ext]",
+            emitFile: false,
+          },
+        },
       },
       {
         test: /\.(scss|css)$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'postcss-loader', 'sass-loader']
-        })
-      }
-    ]
+          fallback: "style-loader",
+          use: ["css-loader", "postcss-loader", "sass-loader"],
+        }),
+      },
+    ],
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js'}),
-    new ExtractTextPlugin('app.css')
-  ]
+    new VueLoaderPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",
+      filename: "vendor.js",
+    }),
+    new ExtractTextPlugin("app.css"),
+  ],
 };
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
+      "process.env": {
+        NODE_ENV: '"production"',
+      },
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        warnings: false
-      }
-    })
-  ])
+        warnings: false,
+      },
+    }),
+  ]);
 }
