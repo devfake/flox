5bc306ffec0c287ea86ee9a2410bb291d26d63a1
Simounet <contact@simounet.net>
2023-06-20 22:07:24 +0200

    IMDBTest skipped because of cea991bd

--- backend/tests/Services/IMDBTest.php
+++ backend/tests/Services/IMDBTest.php
@@ -13,6 +13,7 @@
     /** @test */
     public function it_should_parse_imdb_rating()
     {
+      $this->markTestSkipped('Web scraping not working any more (commit cea991bd).');
       config(['services.imdb.url' => __DIR__ . '/../Fixtures/imdb/with-rating.html']);
 
       $imdbService = app(IMDB::class);
@@ -25,6 +26,7 @@
     /** @test */
     public function it_should_return_null_if_no_rating_was_found()
     {
+      $this->markTestSkipped('Web scraping not working any more (commit cea991bd).');
       config(['services.imdb.url' => __DIR__ . '/../Fixtures/imdb/without-rating.html']);
 
       $imdbService = app(IMDB::class);
